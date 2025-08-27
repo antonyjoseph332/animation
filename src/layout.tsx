2820de132b1f579bcc30d100c183929dbeebfe8e
“GNPX” <Peter@gnpx.in>
2025-08-27 23:59:02 +0530

    conditions

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,50 +1,56 @@
-import { NavLink, Outlet, useLocation } from 'react-router-dom';
 import gsap from 'gsap';
 import { ScrollSmoother } from 'gsap/ScrollSmoother';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useGSAP } from '@gsap/react';
+import { useState } from 'react';
+import Split from './views/split';
+import Card from './views/card';
+import Track from './views/track';
+import Images from './views/images';
+import Parallax from './views/parallax';
 
 gsap.registerPlugin(ScrollSmoother, ScrollTrigger, useGSAP);
 
 export default function Layout() {
-  const { pathname } = useLocation();
+  const [selected, setSelected] = useState('images');
 
   useGSAP(() => {
     ScrollSmoother.create({
       smooth: 1,
       effects: true,
     });
-  }, [pathname]);
+  }, [selected]);
 
   return (
     <>
       <header className="header">
         <nav>
-          <ul>
-             <li>
-              <NavLink to="/">All</NavLink>
+          <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none', width: '100%', overflowX: 'auto' }}>
+            <li>
+              <button onClick={() => setSelected('images')}>Images</button>
             </li>
             <li>
-              <NavLink to="/image">Images</NavLink>
+              <button onClick={() => setSelected('card')}>Card</button>
             </li>
             <li>
-              <NavLink to="/card">Card</NavLink>
+              <button onClick={() => setSelected('track')}>Track</button>
             </li>
             <li>
-              <NavLink to="/track">Track</NavLink>
+              <button onClick={() => setSelected('parallax')}>Clamp</button>
             </li>
-            {/* <li>
-              <NavLink to="/clamp">Clamp</NavLink>
-            </li> */}
             <li>
-              <NavLink to="/split">Split</NavLink>
+              <button onClick={() => setSelected('split')}>Split</button>
             </li>
           </ul>
         </nav>
       </header>
       <div id="smooth-wrapper">
         <div id="smooth-content">
-          <Outlet />
+          {selected === 'images' && <Images />}
+          {selected === 'card' && <Card />}
+          {selected === 'track' && <Track />}
+          {selected === 'parallax' && <Parallax />}
+          {selected === 'split' && <Split />}
         </div>
       </div>
     </>
